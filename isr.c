0c47ad3fe92925c53a075d4cc714a190fbad4347
prakulhn <112687428+prakulhn@users.noreply.github.com>
2023-10-03 23:58:47 -0700

    Add files via upload

---
+++ isr.c
@@ -0,0 +1,25 @@
+#include <xc.h>
+#include "main.h"
+
+extern unsigned char sec, min;
+void __interrupt() isr (void)
+{
+    static unsigned int count =0; //sec=15
+    if(TMR2IF == 1)
+    {
+        if(++count == 1250) //1sec
+        {
+            count=0;
+            if(sec>0)
+            {
+                sec--;
+            }
+            else if((sec==0)&&(min>0))
+            {
+                min--;
+                sec=59;
+            }
+        }
+        TMR2IF=0;
+    }
+}
